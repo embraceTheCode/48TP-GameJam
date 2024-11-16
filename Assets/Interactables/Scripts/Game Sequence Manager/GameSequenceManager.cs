bb54f73ae0a67d0e2b5428a5b37113b66a6784b8
Ricardo Andres Arriaga Quezada <RicharzoneA28@gmail.com>
2024-11-16 02:53:20 -0600

    Upload Scene

---
+++ Assets/Interactables/Scripts/Game Sequence Manager/GameSequenceManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSequenceManager : MonoBehaviour
+{
+    [SerializeField] public List<SequenceObject> sequenceObject;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
