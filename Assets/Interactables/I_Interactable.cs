d39e6044c3e29b2b72dd0b060d5adbfe063269f6
Ricardo Andres Arriaga Quezada <RicharzoneA28@gmail.com>
2024-11-16 01:21:07 -0600

    Interactable Base Class

---
+++ Assets/Interactables/I_Interactable.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface I_Interactable
+{
+    // Interact with object
+    void Interact();
+    // Reset interaction of the object
+    void ResetInteractable();
+}
