f6d9ee8fdba26e716769481c93967a177fac91a2
Carlos Astengo Macias <embraceTheCod3@gmail.com>
2024-11-16 08:33:41 -0600

    ai prototype finished

---
+++ Assets/Scripts/AI/InteractableTracker.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableTracker : MonoBehaviour
+{
+    public static InteractableTracker Instance;
+    private Stack<InteractionData> _interactables = new ();
+    
+    public bool HasInteractions => _interactables.Count > 0;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    public void RegisterInteraction(InteractionData interactionData)
+    {
+        _interactables.Push(interactionData);
+    }
+    
+    public void UnregisterInteraction()
+    {
+        if (_interactables.Count > 0)
+        {
+            _interactables.Pop();
+        }
+    }
+    
+    public InteractionData GetCurrentlyInteracting()
+    {
+        return _interactables.Peek();
+    }
+}
---
+++ Assets/Scripts/Interactables/testInteraction.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testInteraction : MonoBehaviour
+{
+    [SerializeField] private KeyCode interactKey = KeyCode.E;
+    [SerializeField] private Material highlightMaterial;
+    [SerializeField] private Renderer renderer;
+    private Material defaultMaterial;
+    
+    private void Awake()
+    {
+        defaultMaterial = renderer.material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(interactKey))
+        {
+            Interact();
+            InteractableTracker.Instance.RegisterInteraction(new InteractionData(this, transform, "Interact"));
+        }
+    }
+    
+    private void Interact()
+    {
+        Debug.Log("Interacted with " + gameObject.name);
+        renderer.material = highlightMaterial;
+    }
+    
+    public void Reset()
+    {
+        Debug.Log("Reset " + gameObject.name);
+        renderer.material = defaultMaterial;
+    }
+}
